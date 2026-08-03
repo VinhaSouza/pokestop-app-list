ffae4191df9ce96989a9d98e60826718e6935c13
VinhaSouza <wdflaviia@gmail.com>
2026-08-03 16:47:01 -0300

    fix: removido importações inutilizadas

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,4 +1,4 @@
-import { ActivityIndicator, StyleSheet, Text, TextInput, TextInputProps, TouchableHighlightProps, TouchableOpacity } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, TouchableHighlightProps, TouchableOpacity } from 'react-native'
 
 type Props = TouchableHighlightProps & {
     text:string,
