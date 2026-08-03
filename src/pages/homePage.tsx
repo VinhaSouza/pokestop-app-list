87d3cf6b207202f2a487ddb613777f51f0540bc9
VinhaSouza <wdflaviia@gmail.com>
2026-08-03 15:15:09 -0300

    feat: criada a base da tela Principal

---
+++ src/pages/homePage.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, StyleSheet } from 'react-native'
+
+export default function HomePage () {
+    return (
+        <Text>Hello</Text>
+    )
+}
+
+const style = StyleSheet.create({
+
+})
