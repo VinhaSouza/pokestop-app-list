e249e5676bd25e5b1c61d023ecd86cbb66e7a000
VinhaSouza <wdflaviia@gmail.com>
2026-08-03 16:54:26 -0300

    feat: adicionada funcionalidade de navegação principal

---
+++ src/routes/index.routes.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import LoginPage from "../pages/loginpage";
+import HomeRoutes from "./home.routes";
+
+export default function Routes() {
+    const Stack = createStackNavigator();
+    
+    return(
+        <Stack.Navigator
+            initialRouteName="Login"
+            screenOptions={{
+                headerShown: false,
+                cardStyle: {
+                    backgroundColor: 'white',
+                }
+            }}
+        >
+            <Stack.Screen
+                name="Login"
+                component={LoginPage}
+            />
+            <Stack.Screen
+                name="HomeRoutes"
+                component={HomeRoutes}
+            />
+
+        </Stack.Navigator>
+
+    )
+}
