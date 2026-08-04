c43cde969def18548376e5563dc307ac26d548a6
VinhaSouza <wdflaviia@gmail.com>
2026-08-04 10:43:00 -0300

    feat: criado componente de produto único

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,51 @@
+import { Image, StyleSheet, Text, View } from 'react-native'
+
+interface Props {
+    name: string;
+    image: string;
+    price: string;
+}
+
+export function ProductCard({ name, image, price }: Props) {
+    return (
+        <View style={style.cardsProcuct}>
+            <Image
+                source={{uri: image}}
+                style={style.imageProduct}
+                resizeMode='contain'
+            />
+
+            <Text style={style.nameProduct}>{name}</Text>
+            <Text style={style.priceProduct}>{price}</Text>
+        </View>
+    )
+}
+
+const style = StyleSheet.create ({
+    cardsProcuct: {
+        backgroundColor: '#fff',
+        width: "48%",
+        borderRadius: 12,
+        padding: 12,
+        marginBottom: 20,
+        alignItems: "center",
+        elevation: 4
+
+    },
+    imageProduct: {
+        width: 100,
+        height: 100,
+        marginBottom: 10
+    },
+    nameProduct: {
+        fontSize: 16,
+        fontWeight: '600',
+        textAlign: 'center',
+    },
+    priceProduct: {
+        color: '#E53935',
+        marginTop: 5,
+        fontWeight: 'bold',
+        fontSize: 16
+    }
+})
