262fd89d8f00dcd581e5030bc471ab96ceaa704e
VinhaSouza <wdflaviia@gmail.com>
2026-08-04 16:05:16 -0300

    feat: criada a lista com os dados de preços

---
+++ src/data/prices.ts
@@ -0,0 +1,27 @@
+export const prices: Record<string, number> = {
+    "poke-ball": 200,
+    "love-ball": 300,
+    "great-ball": 600,
+    "ultra-ball": 1200,
+    "master-ball": 5000,
+
+    "potion": 300,
+    "super-potion": 700,
+    "hyper-potion": 1200,
+    "max-potion": 3000,
+
+    "repel": 250,
+    "super-repel": 500,
+    "max-repel": 1000,
+
+    "oran-berry": 200,
+    "sitrus-berry": 400,
+    "lum-berry": 700,
+
+    "revive": 300,
+    "max-revive": 600,
+
+    "fire-stone": 1200,
+    "water-stone": 1300,
+    "thunder-stone": 1400,
+}
