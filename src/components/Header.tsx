03c637d039a6a89843b6c48d58583d0db1a9420c
VinhaSouza <wdflaviia@gmail.com>
2026-08-06 17:55:33 -0300

    refactor: ajustado componentização de texto

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native';
-import MaterialIcons from '@react-native-vector-icons/material-icons';
-import { useNavigation, NavigationProp } from '@react-navigation/native';
-import { ButtonIcon } from './ButtonIcon';
+import { StyleSheet, Text, TextProps } from 'react-native';
 
-export function Header() {
-    const navigation = useNavigation<NavigationProp<any>>();
+type Props = TextProps & {
+    text: string,
+}
+
+export function Header({...rest}: Props) {
     return (
-        <View style={style.container}>
-            <ButtonIcon
-                style={style.boxIcon}
-                text=''
-                onPress={() => navigation.navigate('Login')}
-                icon={
-                    <MaterialIcons name='arrow-back' size={32}/>
-                }
-            />
-      
-            <Text style={style.title}>
-                PRODUTOS{"\n"}EM DESTAQUE
-            </Text>
-
-            <ButtonIcon
-                style={style.boxIcon}
-                text=''
-                onPress={() => navigation.navigate('')}
-                icon={
-                    <MaterialIcons name='shopping-cart' size={32}/>
-                }
-            />
-        </View>
+            <Text style={style.title} >
+                {rest.text}
+            </Text> 
     )
 }
 
 const style = StyleSheet.create({
-    container: {
-        backgroundColor: '#c22525',
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        alignItems: 'center',
-        marginTop: 40,
-    },
-    boxIcon:{
-        backgroundColor: 'white',
-        borderRadius: '100%',
-        padding: 6,
-    },
     title: {
         fontSize: 24,
         padding: 10,
