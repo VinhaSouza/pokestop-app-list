6ff999502c51beb4e873f13fbcb4783fa09a904b
VinhaSouza <wdflaviia@gmail.com>
2026-08-06 14:45:46 -0300

    refactor: corrigida a importação da tela de login

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginPage from "../pages/loginpage";
+import LoginPage from "../pages/LoginPage";
 import HomeRoutes from "./home.routes";
 
 export default function Routes() {
