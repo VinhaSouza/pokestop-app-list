edfe8dfea1dfc8ca4511c5e8cfb16d5305a8a4a1
VinhaSouza <wdflaviia@gmail.com>
2026-08-06 14:37:43 -0300

    feat: criada a estrutura base da tela de detalhes dos produtos e imprimindo dados da api

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,9 +1,57 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { RootStackParamList } from '../@types/routes';
+import { api } from '../services/api';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+import { ButtonIcon } from '../components/ButtonIcon';
+import { useNavigation, NavigationProp, useRoute, RouteProp } from '@react-navigation/native';
 
 export default function ProductDetailPage () {
+    const navigation = useNavigation<NavigationProp<any>>()
+    const route = useRoute<RouteProp<RootStackParamList, 'ProductDetail'>>();
+    const { name } = route.params;
+    const { price } = route.params;
+    const { image } = route.params;
+    const [item, setItem] = useState<any>('');
+
+    async function getItems() {
+        const response = await api.get(`/item/${name}`);
+        setItem(response.data)
+    }
+    useEffect(() => {
+        getItems();
+    },)
+
     return (
         <View style={style.container}>
-            <Text>Os detalhes vem aqui!</Text>
+            <View style={style.imageBox}>
+                <Image
+                    source={{uri: image}}
+                    style={style.image}
+                />
+            </View>
+
+            <View style={style.nameBox}>
+                <Text style={style.name}>{name}</Text>
+            </View>
+
+            <View style={style.priceBox}>
+                <Text style={style.name}>{price}</Text>
+            </View>
+
+            <ButtonIcon style={style.addButton} text="" onPress={() => navigation.navigate('List')}
+                icon={<MaterialIcons name='add' size={40}/>}
+            />
+
+            <View style={style.descriptionBox}>
+                <Text style={style.description}>
+                    {item?.effect_entries?.
+                        find(
+                            (text: any) => text.language.name === 'en'
+                        )?.effect
+                    }
+                </Text>
+            </View>
         </View>
     )
 }
@@ -13,5 +61,33 @@ const style = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
+    },
+    imageBox: {
+
+    },
+    image: {
+        width: 150,
+        height: 150
+    },
+    nameBox: {
+
+    },
+    name: {
+
+    },
+    priceBox: {
+
+    },
+    price: {
+
+    },
+    addButton: {
+
+    },
+    descriptionBox: {
+
+    },
+    description: {
+
     },
 })
