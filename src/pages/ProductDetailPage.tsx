6d3d87d8f3416413af6455b84127b05dd012d7d2
VinhaSouza <wdflaviia@gmail.com>
2026-08-06 17:58:04 -0300

    style: estilização na tela de detalhes

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -2,9 +2,10 @@ import React, { useEffect, useState } from 'react';
 import { Image, StyleSheet, Text, View } from 'react-native';
 import { RootStackParamList } from '../@types/routes';
 import { api } from '../services/api';
-import MaterialIcons from '@react-native-vector-icons/material-icons';
 import { ButtonIcon } from '../components/ButtonIcon';
 import { useNavigation, NavigationProp, useRoute, RouteProp } from '@react-navigation/native';
+import { formatName } from '../utils/formatName';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function ProductDetailPage () {
     const navigation = useNavigation<NavigationProp<any>>()
@@ -23,36 +24,35 @@ export default function ProductDetailPage () {
     },)
 
     return (
-        <View style={style.container}>
-            <View style={style.imageBox}>
-                <Image
-                    source={{uri: image}}
-                    style={style.image}
-                />
-            </View>
+         <LinearGradient
+                colors={['#ee7181', '#eec7c7']}
+                start={{x: 0.85, y: 0.85}}
+                end={{x: 0.15, y: 0.15}}
+                style={style.container}
+            >
+            <View style={style.container}>
+                <View style={style.imageBox}>
+                    <Image
+                        source={{uri: image}}
+                        style={style.image}
+                    />
+                </View>
 
-            <View style={style.nameBox}>
-                <Text style={style.name}>{name}</Text>
-            </View>
+                <View style={style.descriptionBox}>
+                    <Text style={style.name}>{formatName(name)}</Text>
+            
+                    <Text style={style.price}> G {price}</Text>
 
-            <View style={style.priceBox}>
-                <Text style={style.name}>{price}</Text>
+                    <Text> - Quantidade + </Text> 
+           
+                    <ButtonIcon style={style.addButton} text="ADICIONAR AO CARRINHO" onPress={() => navigation.navigate('ShoppingCart')}/>
+            
+                    <Text style={style.description}>
+                        {item?.effect_entries?.find((text: any) => text.language.name === 'en')?.effect}
+                    </Text>
+                </View>
             </View>
-
-            <ButtonIcon style={style.addButton} text="" onPress={() => navigation.navigate('List')}
-                icon={<MaterialIcons name='add' size={40}/>}
-            />
-
-            <View style={style.descriptionBox}>
-                <Text style={style.description}>
-                    {item?.effect_entries?.
-                        find(
-                            (text: any) => text.language.name === 'en'
-                        )?.effect
-                    }
-                </Text>
-            </View>
-        </View>
+        </LinearGradient>
     )
 }
 
@@ -60,34 +60,46 @@ const style = StyleSheet.create({
     container:{
         flex: 1,
         alignItems: 'center',
-        justifyContent: 'center'
+        justifyContent: 'center',
     },
     imageBox: {
-
+        backgroundColor: 'white',
+        padding: 50,
+        borderRadius: 50,
+        elevation: 10,
     },
     image: {
-        width: 150,
-        height: 150
-    },
-    nameBox: {
-
-    },
-    name: {
-
-    },
-    priceBox: {
-
-    },
-    price: {
-
-    },
-    addButton: {
-
+        width: 200,
+        height: 200
     },
     descriptionBox: {
-
+        alignItems: 'center',
+        paddingVertical: 20,
+    },
+    name: {
+        fontSize: 30,
+        fontWeight: 'bold',
+        marginBottom: 5,
+    },
+    price: {
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'red',
+        marginBottom: 15,
+    },
+    addButton: {
+        margin: 10,
+        backgroundColor: '#c22525',
+        padding: 12,
+        width: 200,
+        borderRadius: 60,
+        alignItems: 'center',
     },
     description: {
-
+        textAlign: 'justify',
+        fontSize:20,
+        fontStyle: 'italic',
+        paddingHorizontal: 20,
+        paddingVertical: 20,
     },
 })
