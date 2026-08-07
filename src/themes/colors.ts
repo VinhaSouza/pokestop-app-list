0b8b8111c8c9730a126d0c06c1eb087d245412cb
VinhaSouza <wdflaviia@gmail.com>
2026-08-07 16:42:22 -0300

    feat: criado arquivos de estilos globais

---
+++ src/themes/colors.ts
@@ -0,0 +1,20 @@
+export const colors = {
+    white: "#FFFFFF",
+    black: "#000000",
+
+    background: "#8e0114",
+    backgroundsec: "#f06666",
+    lightBackground: "#ee7181",
+    lightBackgroundSec: "#eec7c7",
+
+
+    defaulttext: "#E53935",
+    colorIcon: "#FFFFFF",
+
+    primary: "#c22525",
+    secondary: "#722020",
+
+    inputBorder: "#d97c7c",
+    inputIcon: "#a5a3a3",
+    inputBackground: "#eeeded",
+}
