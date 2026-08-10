53b1adc7052049146e69d81c9fdd2bb393d68f73
VinhaSouza <wdflaviia@gmail.com>
2026-08-10 15:03:36 -0300

    refactor: adicionado cartcontext globalmente

--- App.tsx
+++ App.tsx
@@ -1,12 +1,15 @@
 import './gesture-handler';
+import { CartProvider } from './src/contexts/CartContext';
 import Routes from './src/routes/index.routes';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Routes/>
-    </NavigationContainer>
+    <CartProvider>
+      <NavigationContainer>
+        <Routes/>
+      </NavigationContainer>
+    </CartProvider>
     
   );
 }
