d030ed55f15599bb071094a2b3af40ee01180ee8
VinhaSouza <wdflaviia@gmail.com>
2026-08-11 12:20:01 -0300

    chore: ajustadas variáveis de ambiente das apis para o expo

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,9 @@
 import {create} from "axios";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 export const api = create({
-    baseURL: process.env.DB_BASEURL_API
+    baseURL: process.env.EXPO_PUBLIC_URL_API
 });
 
 export const backendApi = create({
-    baseURL: process.env.DB_BASEURL_MYAPI
+    baseURL: process.env.EXPO_PUBLIC_URL_MYAPI
 });
