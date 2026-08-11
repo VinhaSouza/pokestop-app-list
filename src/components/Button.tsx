76c4621c8102cadc52b7002d40346dde1d02c64b
VinhaSouza <wdflaviia@gmail.com>
2026-08-11 14:44:08 -0300

    feat: adicionado estado de carregamento ao componente botão

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,26 @@
-import { ActivityIndicator, StyleSheet, Text, TouchableOpacityProps, TouchableOpacity } from 'react-native';
+import { ActivityIndicator, StyleSheet, Text, View, TouchableOpacityProps, TouchableOpacity } from 'react-native';
 import { colors } from '../themes/colors';
+import { iconSizes } from '../themes/iconSizes';
 
 type Props = TouchableOpacityProps & {
     text:string,
     loading?:boolean
 }
 
-export function Button({...rest}:Props){
+export function Button({loading,...rest}:Props){
     return(
         <TouchableOpacity
             {...rest}
             activeOpacity={0.6}
+            disabled={loading}
         >
-            {rest.loading?<ActivityIndicator/>:<Text style={style.buttonTitle}>{rest.text}</Text>}
+            {loading ? ( 
+                <View style={style.loading}>
+                <ActivityIndicator size={iconSizes.small}/>
+                <Text style={style.buttonTitle}>Entrando...</Text>    
+                </View>
+            ) : 
+            (<Text style={style.buttonTitle}>{rest.text}</Text>)}
         </TouchableOpacity>
     )
 }
@@ -22,5 +30,8 @@ const style = StyleSheet.create({
         fontSize: 16,
         color: colors.white,
         fontWeight: '600',
+    },
+    loading: {
+        flexDirection: 'row',
     }
 })
