caee9bf20841689fc333646850da2f5cff0cc58b
VinhaSouza <wdflaviia@gmail.com>
2026-08-11 17:36:55 -0300

    style: organizado estrutura de hierarquia

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import './gesture-handler';
 import { CartProvider } from './src/contexts/CartContext';
+import { ProductsProvider } from './src/contexts/ProductsContext';
 import Routes from './src/routes/index.routes';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
     <CartProvider>
-      <NavigationContainer>
-        <Routes/>
-      </NavigationContainer>
+      <ProductsProvider>
+        <NavigationContainer>
+          <Routes/>
+        </NavigationContainer>
+      </ProductsProvider>
     </CartProvider>
   );
 }
