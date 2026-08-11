d63b8098c333a63ccee23f31ddcea4b74da715c3
VinhaSouza <wdflaviia@gmail.com>
2026-08-11 10:20:09 -0300

    style: corrigida  a importação da loginpage

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import LoginPage from "../pages/LoginPage";
+import LoginPage from "../pages/Loginpage";
 import HomeRoutes from "./home.routes";
 
 export default function Routes() {
