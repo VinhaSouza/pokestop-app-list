472b68aedacb61782f00d4ae6a8276d5b7d3bd5e
VinhaSouza <wdflaviia@gmail.com>
2026-08-12 16:22:20 -0300

    refactor: removido productprovider que envolvia todo o app

--- App.tsx
+++ App.tsx
@@ -1,17 +1,14 @@
 import './gesture-handler';
 import { CartProvider } from './src/contexts/CartContext';
-import { ProductsProvider } from './src/contexts/ProductsContext';
 import Routes from './src/routes/index.routes';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
   return (
-    <CartProvider>
-      <ProductsProvider>
-        <NavigationContainer>
-          <Routes/>
-        </NavigationContainer>
-      </ProductsProvider>
+    <CartProvider>  
+      <NavigationContainer>
+        <Routes/>
+      </NavigationContainer>
     </CartProvider>
   );
 }
