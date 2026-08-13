68486c712ee5cbec4831f3ee41c025bfdc529be8
VinhaSouza <wdflaviia@gmail.com>
2026-08-13 11:00:16 -0300

    feat: adicionada tela de registro nas rotas principais

--- src/routes/index.routes.tsx
+++ src/routes/index.routes.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import LoginPage from "../pages/Loginpage";
 import HomeRoutes from "./home.routes";
+import RegisterPage from "../pages/RegisterPage";
 
 export default function Routes() {
     const Stack = createStackNavigator();
@@ -23,6 +24,10 @@ export default function Routes() {
             <Stack.Screen
                 name="HomeRoutes"
                 component={HomeRoutes}
+            />
+            <Stack.Screen
+                name="Register"
+                component={RegisterPage}
             />
 
         </Stack.Navigator>
