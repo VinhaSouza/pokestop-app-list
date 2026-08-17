97a1bbd878531f5de3fde956a9f0ea07b8134e2c
VinhaSouza <wdflaviia@gmail.com>
2026-08-17 10:44:03 -0300

    feat: criado componente da barra de pesquisa

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
+import MaterialIcons from '@react-native-vector-icons/material-icons';
+import { StyleSheet, TextInput, TouchableOpacity, View } from 'react-native';
+import { iconSizes } from '../themes/iconSizes';
+import { colors } from '../themes/colors';
 
 interface SearchBarProps {
     value: string;
     onChangeText: (text: string) => void;
-    icon?: React.ReactNode;
+    onFilterPress: () => void;
 }
 
-export default function SearchBar() {
-    return;
+export default function SearchBar({ value, onChangeText, onFilterPress }: SearchBarProps) {
+    return (
+        <View style={styles.container}>
+            <MaterialIcons name="search" size={iconSizes.meddium} style={styles.icon} />
+            <TextInput
+                style={styles.input}
+                value={value}
+                onChangeText={onChangeText}
+                placeholder="Pesquisar produto..."
+            />
+            <TouchableOpacity onPress={onFilterPress}>
+                <MaterialIcons name="filter-list" size={iconSizes.meddium} style={styles.icon} />
+            </TouchableOpacity>
+        </View>
+    );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        width: '90%',
+        height: 50,
+        borderWidth: 2,
+        borderRadius: 40,
+        borderColor: colors.primary,
+        paddingHorizontal: 12,
+        marginHorizontal: 20,
+        marginVertical: 10,
+    },
+    icon: {
+        marginRight: 8,
+        color: colors.primary,
+    },
+    input: {
+        flex: 1,
+        height: '100%',
+    },
+});
