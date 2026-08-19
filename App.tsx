32b802814f81072ef292a6c1e4c92a9846937c48
VinhaSouza <wdflaviia@gmail.com>
2026-08-19 14:04:42 -0300

    refactor: adicionado authprovider globalmente no projeto

--- App.tsx
+++ App.tsx
@@ -1,14 +1,17 @@
 import './gesture-handler';
+import { AuthProvider } from './src/contexts/AuthContext';
 import { CartProvider } from './src/contexts/CartContext';
 import Routes from './src/routes/index.routes';
 import { NavigationContainer } from '@react-navigation/native';
 
 export default function App() {
     return (
-        <CartProvider>
-            <NavigationContainer>
-                <Routes />
-            </NavigationContainer>
-        </CartProvider>
+        <AuthProvider>
+            <CartProvider>
+                <NavigationContainer>
+                    <Routes />
+                </NavigationContainer>
+            </CartProvider>
+        </AuthProvider>
     );
 }
