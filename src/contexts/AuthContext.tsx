4f68dbd68348ac863e8259aeeb446b01c24153aa
VinhaSouza <wdflaviia@gmail.com>
2026-08-19 13:47:46 -0300

    feat: criado contexto de autenticação de usuários

---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,22 @@
+import React, { createContext, ReactNode, useState } from 'react';
+
+interface User {
+    id: number;
+    name: string;
+}
+
+interface AuthContextData {
+    user: User | null;
+    setUser: (user: User | null) => void;
+}
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+export const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderProps) {
+    const [user, setUser] = useState<User | null>(null);
+
+    return <AuthContext.Provider value={{ user, setUser }}>{children}</AuthContext.Provider>;
+}
