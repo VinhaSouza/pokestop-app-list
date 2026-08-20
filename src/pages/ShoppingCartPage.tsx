38369a6c0f95e167d08ef2bf9b474a9fd9a11cd7
VinhaSouza <wdflaviia@gmail.com>
2026-08-20 08:27:15 -0300

    refactor: integrado carrinho com a api, formatação do código e estilização atualizada

--- src/pages/ShoppingCartPage.tsx
+++ src/pages/ShoppingCartPage.tsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { View, StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, Text, ScrollView } from 'react-native';
 import { Header } from '../components/Header';
 import { ButtonIcon } from '../components/ButtonIcon';
 import MaterialIcons from '@react-native-vector-icons/material-icons';
@@ -9,40 +9,90 @@ import { FlatList } from 'react-native-gesture-handler';
 import { colors } from '../themes/colors';
 import { formatName } from '../utils/formatName';
 import { Button } from '../components/Button';
+import { iconSizes } from '../themes/iconSizes';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function ShoppingCartPage() {
-    const { cart, clearCart } = useContext(CartContext);
+    const { cart, clearCart, addToCart, decrease } = useContext(CartContext);
     const totalPrice = cart.reduce((total, item) => total + item.price * item.quantity, 0);
     const navigation = useNavigation<NavigationProp<any>>();
     return (
-        <View style={style.container}>
-            <View style={style.containerHeader}>
-                <ButtonIcon
-                    text=""
-                    onPress={() => navigation.navigate('List')}
-                    icon={<MaterialIcons name="arrow-back" size={30} color="white" />}
-                />
+        <SafeAreaView style={style.container}>
+            <ScrollView style={style.scrollView}>
+                <View style={style.containerHeader}>
+                    <ButtonIcon
+                        text=""
+                        onPress={() => navigation.navigate('List')}
+                        icon={<MaterialIcons name="arrow-back" size={30} color="white" />}
+                    />
 
-                <Header text="Seu Carrinho" />
+                    <Header text="Seu Carrinho" />
+
+                    <ButtonIcon
+                        text=""
+                        onPress={() => navigation.navigate('List')}
+                        icon={<MaterialIcons name="account-circle" size={30} color="white" />}
+                    />
+                </View>
+                <View style={style.listContainer}>
+                    <FlatList
+                        data={cart}
+                        keyExtractor={(item) => item.name}
+                        scrollEnabled={false}
+                        renderItem={({ item }) => (
+                            <View style={style.dataCard}>
+                                <Text style={style.itemName}>{formatName(item.name)}</Text>
+
+                                <View style={style.countBox}>
+                                    <Text style={style.countTitle}>Quantidade:</Text>
+                                    <View style={style.counter}>
+                                        <ButtonIcon
+                                            text=""
+                                            onPress={() => addToCart(item)}
+                                            icon={
+                                                <MaterialIcons
+                                                    style={style.buttons}
+                                                    name="add"
+                                                    size={iconSizes.meddium}
+                                                />
+                                            }
+                                        />
+
+                                        <Text style={style.itemQnt}>{item.quantity}</Text>
+
+                                        <ButtonIcon
+                                            text=""
+                                            onPress={() => decrease(item.name)}
+                                            icon={
+                                                <MaterialIcons
+                                                    style={style.buttons}
+                                                    name="remove"
+                                                    size={iconSizes.meddium}
+                                                />
+                                            }
+                                        />
+                                    </View>
+                                </View>
+
+                                <Text style={style.itemPrice}>Preço unitário: G {item.price}</Text>
+                                <Text style={style.itemTotal}>
+                                    Total: G {item.price * item.quantity}
+                                </Text>
+                            </View>
+                        )}
+                    />
+                    <Button
+                        style={style.clearButton}
+                        text="Limpar o Carrinho"
+                        onPress={clearCart}
+                    />
+                </View>
+            </ScrollView>
+            <View style={style.footer}>
+                <Text style={style.totalTitle}>Total do Carrinho: G {totalPrice}</Text>
+                <Button style={style.shopButton} text="Finalizar Compra" />
             </View>
-            <View style={style.listContainer}>
-                <FlatList
-                    data={cart}
-                    keyExtractor={(item) => item.name}
-                    renderItem={({ item }) => (
-                        <View style={style.dataCard}>
-                            <Text style={style.itemName}>{formatName(item.name)}</Text>
-                            <Text style={style.itemQnt}>Quantidade: {item.quantity}</Text>
-                            <Text style={style.itemPrice}>Preço UN: {item.price}</Text>
-                            <Text style={style.itemTotal}>Total: {item.price * item.quantity}</Text>
-                        </View>
-                    )}
-                />
-                <Text>Total do carrinho</Text>
-                <Text>{totalPrice}</Text>
-                <Button style={style.clearButton} text="Limpar o Carrinho" onPress={clearCart} />
-            </View>
-        </View>
+        </SafeAreaView>
     );
 }
 
@@ -50,12 +100,14 @@ const style = StyleSheet.create({
     container: {
         flex: 1,
     },
+    scrollView: {
+        flexGrow: 1,
+    },
     containerHeader: {
         backgroundColor: colors.primary,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-around',
-        marginTop: 40,
         paddingBottom: 10,
     },
     listContainer: {
@@ -64,6 +116,8 @@ const style = StyleSheet.create({
     },
     dataCard: {
         borderWidth: 1,
+        borderRadius: 20,
+        borderColor: colors.gray,
         padding: 10,
         paddingRight: 100,
         marginVertical: 10,
@@ -72,8 +126,34 @@ const style = StyleSheet.create({
     itemName: {
         fontSize: 26,
     },
+    countBox: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
     itemQnt: {
         fontSize: 20,
+        marginHorizontal: 16,
+        paddingHorizontal: 10,
+        borderWidth: 1,
+    },
+    countTitle: {
+        paddingRight: 10,
+    },
+    counter: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    buttons: {
+        width: 30,
+        height: 30,
+        marginBottom: 18,
+        color: colors.white,
+        backgroundColor: colors.primary,
+        borderRadius: 5,
+        elevation: 3,
+    },
+    deleteButton: {
+        alignItems: 'flex-end',
     },
     itemPrice: {
         fontSize: 20,
@@ -82,9 +162,30 @@ const style = StyleSheet.create({
         fontSize: 20,
     },
     clearButton: {
-        backgroundColor: colors.primary,
+        backgroundColor: colors.secondary,
         paddingHorizontal: 20,
         paddingVertical: 10,
         borderRadius: 8,
+        marginLeft: '30%',
+        marginTop: 30,
+    },
+    shopButton: {
+        backgroundColor: 'green',
+        paddingHorizontal: 20,
+        paddingVertical: 10,
+        borderRadius: 8,
+    },
+    totalTitle: {
+        fontSize: 16,
+        fontWeight: 600,
+    },
+    footer: {
+        width: '100%',
+        height: 80,
+        alignItems: 'center',
+        justifyContent: 'space-evenly',
+        flexDirection: 'row',
+        borderTopWidth: 2,
+        borderColor: colors.gray,
     },
 });
