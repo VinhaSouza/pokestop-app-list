f5e0f2b0b99076b841838209d6f11879257259ea
VinhaSouza <wdflaviia@gmail.com>
2026-08-20 11:30:39 -0300

    feat: adicionado bottom tab navigation

--- src/routes/home.routes.tsx
+++ src/routes/home.routes.tsx
@@ -5,8 +5,73 @@ import ProductDetailPage from '../pages/ProductDetailPage';
 import { RootStackParamList } from '../@types/routes';
 import ShoppingCartPage from '../pages/ShoppingCartPage';
 import { ProductsProvider } from '../contexts/ProductsContext';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FavoritePage from '../pages/FavoritePage';
+import ProfilePage from '../pages/ProfilePage';
+import { MaterialIcons } from '@react-native-vector-icons/material-icons';
+import { colors } from '../themes/colors';
+import { iconSizes } from '../themes/iconSizes';
 
 const Stack = createStackNavigator<RootStackParamList>();
+const Tab = createBottomTabNavigator<RootStackParamList>();
+
+function BottomTabs() {
+    return (
+        <Tab.Navigator
+            screenOptions={{
+                headerShown: false,
+            }}>
+            <Tab.Screen
+                name="Home"
+                options={{
+                    title: 'Início',
+                    tabBarIcon: () => (
+                        <MaterialIcons
+                            name="home"
+                            size={iconSizes.meddium}
+                            color={colors.primary}
+                        />
+                    ),
+                }}>
+                {() => (
+                    <ProductsProvider>
+                        <ListPage />
+                    </ProductsProvider>
+                )}
+            </Tab.Screen>
+
+            <Tab.Screen
+                name="Favorites"
+                component={FavoritePage}
+                options={{
+                    title: 'Favoritos',
+                    tabBarIcon: () => (
+                        <MaterialIcons
+                            name="favorite"
+                            size={iconSizes.meddium}
+                            color={colors.primary}
+                        />
+                    ),
+                }}
+            />
+
+            <Tab.Screen
+                name="Profile"
+                component={ProfilePage}
+                options={{
+                    title: 'Perfil',
+                    tabBarIcon: () => (
+                        <MaterialIcons
+                            name="account-circle"
+                            size={iconSizes.meddium}
+                            color={colors.primary}
+                        />
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
 
 export default function HomeRoutes() {
     return (
@@ -17,13 +82,7 @@ export default function HomeRoutes() {
                     backgroundColor: 'white',
                 },
             }}>
-            <Stack.Screen name="List">
-                {() => (
-                    <ProductsProvider>
-                        <ListPage />
-                    </ProductsProvider>
-                )}
-            </Stack.Screen>
+            <Stack.Screen name="BottomTabs" component={BottomTabs} />
 
             <Stack.Screen name="ProductDetail" component={ProductDetailPage} />
 
