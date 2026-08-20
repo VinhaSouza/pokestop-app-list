8ebc4e59f9fa92a788c53c937bd5ea01d800408a
VinhaSouza <wdflaviia@gmail.com>
2026-08-20 14:15:19 -0300

    refactor: movido os modais para a pasta components/modals

--- src/components/modals/ClearCartModal.tsx
+++ src/components/modals/ClearCartModal.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Modal, View, Text, TouchableOpacity, StyleSheet } from 'react-native';
-import { colors } from '../themes/colors';
+import { colors } from '../../themes/colors';
 
 interface ClearCartModalProps {
     visible: boolean;
