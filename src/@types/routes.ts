f8dbb7d7980443b399811e699f3cf3798e62dae4
VinhaSouza <wdflaviia@gmail.com>
2026-08-20 11:29:50 -0300

    refactor: atualizado rootstackparamlist para nova navegação

--- src/@types/routes.ts
+++ src/@types/routes.ts
@@ -1,5 +1,9 @@
 export type RootStackParamList = {
     List: undefined;
+    Home: undefined;
+    Profile: undefined;
+    Favorites: undefined;
+    BottomTabs: undefined;
     ProductDetail: {
         name: string;
         price: number;
